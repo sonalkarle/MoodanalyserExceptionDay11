782baf7725bdbc1addd310886bb38386203188e0
sonal <sonalkarle01@gmail.com>
2021-02-22 19:11:28 +0530

    Welcome

--- MoodanalyserDay11/Moodanalyser.cs
+++ MoodanalyserDay11/Moodanalyser.cs
@@ -1,10 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MoodanalyserDay11
 {
-    class Moodanalyser
+    public class Moodanalyser
     {
+        private string message;
+
+        public Moodanalyser(string message)
+        {
+            this.message = message;
+        }
+        public Moodanalyser()
+        {
+            this.message = "I am in SAD mood";
+        }
+        /// <summary>
+        /// Parametrized constructor
+        /// </summary>
+        /// <returns></returns>
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (this.message.Contains("SAD"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                return "INVALID";
+            }
+        }
     }
 }
+
--- MoodanalyserTest/UnitTest1.cs
+++ MoodanalyserTest/UnitTest1.cs
@@ -1,18 +1,61 @@
 using NUnit.Framework;
+using MoodanalyserDay11;
 
 namespace MoodanalyserTest
 {
     public class Tests
     {
+        Moodanalyser moodanalyser;
         [SetUp]
         public void Setup()
         {
-        }
+          
+            moodanalyser = new Moodanalyser();
 
+        }
+        /// <summary>
+        /// UC1.1: Given message "I am in SAD mood" returns SAD
+        /// </summary>
         [Test]
-        public void Test1()
+        public void Given_message_In_SAD_Mood_return_SAD_message()
         {
-            Assert.Pass();
+            //Arrange
+            string message = "I am in SAD Mood";
+            moodanalyser = new Moodanalyser(message);
+            //Act
+            string expected = "SAD";
+            string actual = moodanalyser.AnalyseMood();
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        ///<summary>
+        ///UC1.2: Given message "I am in any mood" return HAPPY
+        ///</summary>
+        [Test]
+        public void Given_message_return_In_Any_Mood_return_Happy_message()
+        {
+            //Arrange
+            string message = "I am in Any Mood";
+            moodanalyser = new Moodanalyser(message);
+            //Act
+            string expected = "HAPPY";
+            string actual = moodanalyser.AnalyseMood();
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        ///<summary>
+        ///UC1.1:Handle null exception return as invalid
+        /// </summary>
+        [Test]
+        public void Handle_NullException_return_Invalid()
+        {
+            string message = " ";
+           moodanalyser = new Moodanalyser(message);
+            string expected = null;
+            string actual = moodanalyser.AnalyseMood();
+            Assert.AreEqual(expected, actual);
+            
         }
     }
 }
