88b109099c4419d8b5456435a03077c2e601e7dd
sonal <sonalkarle01@gmail.com>
2021-02-25 15:58:44 +0530

    Handle the mood custom Exception

--- MoodanalyserDay11/Moodanalyser.cs
+++ MoodanalyserDay11/Moodanalyser.cs
@@ -5,19 +5,23 @@ namespace MoodanalyserDay11
     public class Moodanalyser
     {
         private string message;
-
-        public Moodanalyser(string message)
-        {
-            this.message = message;
-        }
-        public Moodanalyser()
-        {
-            this.message = "I am in SAD mood";
-        }
         /// <summary>
         /// Parametrized constructor
         /// </summary>
         /// <returns></returns>
+        public Moodanalyser(string message)
+        {
+            this.message = message;
+        }
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <returns></returns>
+        public Moodanalyser()
+        {
+            this.message = "I am in SAD mood";
+        }
+      
         public string AnalyseMood()
         {
             try
